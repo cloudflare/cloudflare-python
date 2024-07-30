0a295d2fc0f20dc6c3cfec5fe4f7538f99791248
Aji Mufti <39007244+mftzk@users.noreply.github.com>
2024-07-30 13:59:20 +0700

    Update record.py
    move variable to record kwargs

--- examples/dns/record.py
+++ examples/dns/record.py
@@ -6,9 +6,6 @@ zone_id = os.getenv("CLOUDFLARE_ZONE_ID")
 if zone_id is None:
     sys.exit("CLOUDFLARE_ZONE_ID is not defined")
 
-alias_name = "www.mydns.com"
-cannonical_name = "cname.example.com"
-
 
 client = Cloudflare()
 
@@ -17,8 +14,8 @@ client = Cloudflare()
 record = client.dns.records.create(
     zone_id=zone_id, 
     type='CNAME', 
-    name=alias_name, 
-    content=cannonical_name,
+    name="www.mydns.com", 
+    content="cname.example.com",
     proxied=False
     )
 
