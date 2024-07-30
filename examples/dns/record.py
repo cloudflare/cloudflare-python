4002758021f382fc8f5fba50d4c4e230b4b91d11
Aji Mufti <39007244+mftzk@users.noreply.github.com>
2024-07-30 13:55:17 +0700

    Update record.py
    zone id get from os environment variable

--- examples/dns/record.py
+++ examples/dns/record.py
@@ -1,24 +1,27 @@
+import os
 from cloudflare import Cloudflare
 
 
+zone_id = os.getenv("CLOUDFLARE_ZONE_ID")
+if zone_id is None:
+    sys.exit("CLOUDFLARE_ZONE_ID is not defined")
 
-cloudflare_token = "your-cloudflare-token"
-cloudflare_zone_id = "zone"
 alias_name = "www.mydns.com"
 cannonical_name = "cname.example.com"
+
 
 client = Cloudflare()
 
 
-
 # create dns using defined driver
 record = client.dns.records.create(
-    zone_id=cloudflare_zone_id, 
+    zone_id=zone_id, 
     type='CNAME', 
     name=alias_name, 
     content=cannonical_name,
     proxied=False
     )
+
 
 # print output
 print(record)
