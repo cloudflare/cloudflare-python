a2c3d0c5ce01f5a4e4d43bab7289ac85ca2a27e2
Musa Jundi <musa@cloudflare.com>
2026-04-17 07:32:54 -0500

    feat(certificate_authorities): update generated types and methods

--- src/cloudflare/resources/certificate_authorities/hostname_associations.py
+++ src/cloudflare/resources/certificate_authorities/hostname_associations.py
@@ -7,7 +7,7 @@ from typing import Type, Optional, cast
 import httpx
 
 from ..._types import Body, Omit, Query, Headers, NotGiven, SequenceNotStr, omit, not_given
-from ..._utils import maybe_transform, async_maybe_transform
+from ..._utils import path_template, maybe_transform, async_maybe_transform
 from ..._compat import cached_property
 from ..._resource import SyncAPIResource, AsyncAPIResource
 from ..._response import (
@@ -49,7 +49,7 @@ class HostnameAssociationsResource(SyncAPIResource):
     def update(
         self,
         *,
-        zone_id: str,
+        zone_id: str | None = None,
         hostnames: SequenceNotStr[HostnameAssociation] | Omit = omit,
         mtls_certificate_id: str | Omit = omit,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
@@ -77,10 +77,12 @@ class HostnameAssociationsResource(SyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
+        if zone_id is None:
+            zone_id = self._client._get_zone_id_path_param()
         if not zone_id:
             raise ValueError(f"Expected a non-empty value for `zone_id` but received {zone_id!r}")
         return self._put(
-            f"/zones/{zone_id}/certificate_authorities/hostname_associations",
+            path_template("/zones/{zone_id}/certificate_authorities/hostname_associations", zone_id=zone_id),
             body=maybe_transform(
                 {
                     "hostnames": hostnames,
@@ -103,7 +105,7 @@ class HostnameAssociationsResource(SyncAPIResource):
     def get(
         self,
         *,
-        zone_id: str,
+        zone_id: str | None = None,
         mtls_certificate_id: str | Omit = omit,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
         # The extra values given here take precedence over values defined on the client or passed to this method.
@@ -130,10 +132,12 @@ class HostnameAssociationsResource(SyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
+        if zone_id is None:
+            zone_id = self._client._get_zone_id_path_param()
         if not zone_id:
             raise ValueError(f"Expected a non-empty value for `zone_id` but received {zone_id!r}")
         return self._get(
-            f"/zones/{zone_id}/certificate_authorities/hostname_associations",
+            path_template("/zones/{zone_id}/certificate_authorities/hostname_associations", zone_id=zone_id),
             options=make_request_options(
                 extra_headers=extra_headers,
                 extra_query=extra_query,
@@ -172,7 +176,7 @@ class AsyncHostnameAssociationsResource(AsyncAPIResource):
     async def update(
         self,
         *,
-        zone_id: str,
+        zone_id: str | None = None,
         hostnames: SequenceNotStr[HostnameAssociation] | Omit = omit,
         mtls_certificate_id: str | Omit = omit,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
@@ -200,10 +204,12 @@ class AsyncHostnameAssociationsResource(AsyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
+        if zone_id is None:
+            zone_id = self._client._get_zone_id_path_param()
         if not zone_id:
             raise ValueError(f"Expected a non-empty value for `zone_id` but received {zone_id!r}")
         return await self._put(
-            f"/zones/{zone_id}/certificate_authorities/hostname_associations",
+            path_template("/zones/{zone_id}/certificate_authorities/hostname_associations", zone_id=zone_id),
             body=await async_maybe_transform(
                 {
                     "hostnames": hostnames,
@@ -226,7 +232,7 @@ class AsyncHostnameAssociationsResource(AsyncAPIResource):
     async def get(
         self,
         *,
-        zone_id: str,
+        zone_id: str | None = None,
         mtls_certificate_id: str | Omit = omit,
         # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
         # The extra values given here take precedence over values defined on the client or passed to this method.
@@ -253,10 +259,12 @@ class AsyncHostnameAssociationsResource(AsyncAPIResource):
 
           timeout: Override the client-level default timeout for this request, in seconds
         """
+        if zone_id is None:
+            zone_id = self._client._get_zone_id_path_param()
         if not zone_id:
             raise ValueError(f"Expected a non-empty value for `zone_id` but received {zone_id!r}")
         return await self._get(
-            f"/zones/{zone_id}/certificate_authorities/hostname_associations",
+            path_template("/zones/{zone_id}/certificate_authorities/hostname_associations", zone_id=zone_id),
             options=make_request_options(
                 extra_headers=extra_headers,
                 extra_query=extra_query,
