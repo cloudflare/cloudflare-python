8f54916520e1a909ac1d4f1b304d75cf80a202d1
Musa Jundi <musa@cloudflare.com>
2026-04-23 18:09:55 -0500

    chore(healthchecks): update generated types and methods

--- src/cloudflare/types/healthchecks/healthcheck_edit_params.py
+++ src/cloudflare/types/healthchecks/healthcheck_edit_params.py
@@ -14,7 +14,7 @@ __all__ = ["HealthcheckEditParams"]
 
 
 class HealthcheckEditParams(TypedDict, total=False):
-    zone_id: str
+    zone_id: Required[str]
     """Identifier"""
 
     address: Required[str]
