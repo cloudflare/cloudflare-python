b0eee6b3a650f3cbc0518b6097aa58f591a2a3e3
Musa Jundi <musa@cloudflare.com>
2026-04-23 19:03:38 -0500

    chore(turnstile): update generated types and methods

--- src/cloudflare/types/turnstile/widget_update_params.py
+++ src/cloudflare/types/turnstile/widget_update_params.py
@@ -11,7 +11,7 @@ __all__ = ["WidgetUpdateParams"]
 
 
 class WidgetUpdateParams(TypedDict, total=False):
-    account_id: str
+    account_id: Required[str]
     """Identifier"""
 
     domains: Required[SequenceNotStr[WidgetDomain]]
